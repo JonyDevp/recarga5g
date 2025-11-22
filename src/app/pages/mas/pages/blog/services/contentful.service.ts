5ab895e7188fca2d7de445e54f48e2c69ae07afe
JonyDevp <Jonathansr2029@gmail.com>
2025-11-22 15:21:10 -0600

    fix(blog): fix post when fetching data

--- src/app/pages/mas/pages/blog/services/contentful.service.ts
+++ src/app/pages/mas/pages/blog/services/contentful.service.ts
@@ -1,13 +1,13 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@envs/environment.development';
 import { createClient } from 'contentful';
-import { catchError, from, map, Observable, of, tap, } from 'rxjs';
+import { catchError, from, map, Observable, of, shareReplay, tap, } from 'rxjs';
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 import { TypePostSkeleton } from '../interfaces/content-types';
 import * as Contentful from 'contentful';
 import { MapTagResponse } from '@mas/pages/blog/interfaces/tag-types';
 import { MapImgData, MapPostItem, MapPostResponse } from '../interfaces/post-types';
-import { Router } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
@@ -19,14 +19,50 @@ export class ContentfulService {
     accessToken: environment.contentfull.token,
   });
 
-  private readonly router = inject(Router);
-
-  searchTerm: string = '';
-
-  getPosts(page: number, limit: number = 10): Observable<MapPostResponse> {
-    return from(this.client.getEntries<TypePostSkeleton>({
+  // CACHE
+  private tags$?: Observable<MapTagResponse>;
+  /**
+  * Obtener posts con paginación y filtros opcionales
+  */
+  getPosts(page: number, limit: number = 10, tagId?: string): Observable<MapPostResponse> {
+     
+    const query: any = {
       content_type: 'blogRecarga5g',
       skip: (page - 1) * limit,
+      limit: limit,
+      order: ["-sys.createdAt"],
+      select: ['sys.id', 'fields.title', 'fields.body', 'fields.publishDate', 'fields.slug', 'fields.summary', 'fields.headerImage']
+    };
+
+    // Si hay un tag seleccionado, agregar filtro
+    if (tagId) {
+      query['metadata.tags.sys.id[in]'] = tagId;
+    }
+
+  return from(
+    this.client.getEntries<TypePostSkeleton>(query)
+    ).pipe(
+      map((resp) => {
+        return {
+          total: resp.total,
+          items: resp.items.map(item => this.mapPostItem(item))
+        }
+      }),
+      catchError((error) => {
+        console.error('Error fetching posts', error)
+        return of({ total: 0, items: [] } as MapPostResponse);
+      })
+    );
+
+  ;
+  }
+
+  /**
+   * Obtener posts más recientes (para sidebar o sección destacada)
+   */
+  getCurrentPosts(limit = 3): Observable<MapPostResponse> {
+    return from(this.client.getEntries<TypePostSkeleton>({
+      content_type: 'blogRecarga5g',
       limit: limit,
       order: ["-sys.createdAt"],
       select: ['sys.id', 'fields.title', 'fields.body', 'fields.publishDate', 'fields.slug', 'fields.summary', 'fields.headerImage']
@@ -38,37 +74,23 @@ export class ContentfulService {
           items: resp.items.map(item => this.mapPostItem(item))
         }
       }),
+      catchError((error) => {
+        console.error('Error fetching posts:', error);
+        return of({ total: 0, items: [] } as MapPostResponse);
+      })
 
     )
   }
 
-  getCurrentPosts(): Observable<MapPostResponse> {
-    return from(this.client.getEntries<TypePostSkeleton>({
-      content_type: 'blogRecarga5g',
-      limit: 3,
-      order: ["-sys.createdAt"],
-      select: ['sys.id', 'fields.title', 'fields.body', 'fields.publishDate', 'fields.slug', 'fields.summary', 'fields.headerImage']
-    })
-    ).pipe(
-
-      // tap( (resp) => {
-      //   console.log(resp)
-      // }),
-      map((resp) => {
-        return {
-          total: resp.total,
-          items: resp.items.map(item => this.mapPostItem(item))
-        }
-      }),
-
-    )
-  }
-
+  /**
+  * Obtener un post por ID
+  */
   getPostByID(id: string): Observable<MapPostItem | null> {
-    return from(this.client.getEntry<TypePostSkeleton>(id)).pipe(
-      map((resp: any) => {
-
-        if (!resp) return null;
+    return from(
+      this.client.getEntry<TypePostSkeleton>(id))
+      .pipe(
+        map((resp: any) => {
+         if (!resp) return null;
 
         return {
           id: resp.sys.id,
@@ -78,21 +100,30 @@ export class ContentfulService {
           slug: resp.fields.slug,
           summary: resp.fields.summary,
           headerImage: resp.fields.headerImage ? this.mapImage(resp.fields.headerImage) : null
-        }
+        } as MapPostItem;
+      }),
+      catchError((error) => {
+        console.error('Error fetching post by ID:', error);
+        return of(null)
       })
     )
   }
 
+  /**
+   * Obtener un post por Slug
+   */
   getPostBySlug(slug: string): Observable<MapPostItem | null> {
-    return from(this.client.getEntries({
+    return from(
+      this.client.getEntries({
       content_type: 'blogRecarga5g',
       limit: 1,
-        'fields.slug': slug as any
+      'fields.slug': slug as any
     })
     ).pipe(
       map((resp: any) => {
         if (!resp.items.length) return null; // No se encontró el post
         const post = resp.items[0].fields;
+       
         return {
           id: resp.items[0].sys.id,
           title: post.title,
@@ -101,36 +132,65 @@ export class ContentfulService {
           slug: post.slug,
           summary: post.summary,
           headerImage: post.headerImage ? this.mapImage(post.headerImage) : null,
-        };
+        } as MapPostItem;
 
       }),
-       catchError(() => of(null)) // Manejo de errores
-    )
-  }
-
-  getTags(): Observable<MapTagResponse> {
-    return from(this.client.getTags()).pipe(
-      map((resp) => {
-        return {
-          total: resp.total,
-          skip: resp.skip,
-          items: resp.items.map(item => ({
-            id: item.sys.id,
-            name: item.name
-          }))
-        }
+      catchError((error) => {
+        console.error('Error fetching post by slug:', error);
+        return of(null);
       })
     )
   }
 
+  /**
+ * Obtener todos los tags/categorías
+ */
+  getTags(forceRefresh  = false): Observable<MapTagResponse> {
+    
+    if (!forceRefresh && this.tags$) {
+      return this.tags$;
+    }
+
+    this.tags$ = from(
+      this.client.getTags()
+    ).pipe(
+      map(resp => ({
+        total: resp.total,
+        skip: resp.skip,
+        items: resp.items.map(item => ({
+          id: item.sys.id,
+          name: item.name,
+        })),
+      })),
+      catchError(error => {
+        console.error('Error fetching tags:', error);
+        return of({ total: 0, skip: 0, items: [] } as MapTagResponse);
+      }),
+      shareReplay(1)
+    );
+
+    return this.tags$;    
+  }
+
+  /**
+  * Obtener posts destacados (featured)
+  */
   getFeaturedPost(limit = 3): Observable<MapPostResponse> {
-    return from(this.client.getEntries<any>({
+    return from(
+      this.client.getEntries<any>({
       content_type: 'blogRecarga5g',
       limit: limit,
       'fields.featured': true,
-      order: ["-sys.createdAt"],
-      select: ['sys.id', 'fields.title', 'fields.body', 'fields.publishDate', 'fields.slug', 'fields.summary', 'fields.headerImage'],
-
+      order: ['-sys.createdAt'],
+      select: [
+       'sys.id',
+          'fields.title',
+          'fields.body',
+          'fields.publishDate',
+          'fields.slug',
+          'fields.summary',
+          'fields.headerImage' 
+      ],
     })
     ).pipe(
       map((resp) => {
@@ -139,10 +199,16 @@ export class ContentfulService {
           items: resp.items.map(item => this.mapPostItem(item))
         }
       }),
-
+      catchError((error) => {
+        console.error('Error fetching featured posts:', error);
+        return of({ total: 0, items: [] } as MapPostResponse);
+      })
     )
   }
 
+    /**
+   * Mapear item de post
+   */
   private mapPostItem(item: any): MapPostItem {
     return {
       id: item.sys.id,
@@ -155,6 +221,9 @@ export class ContentfulService {
     };
   }
 
+    /**
+   * Mapear imagen de Contentful
+   */
   private mapImage(asset: Contentful.Asset): MapImgData {
     if (!asset.fields?.file?.url) {
       throw new Error('Invalid asset structure');
