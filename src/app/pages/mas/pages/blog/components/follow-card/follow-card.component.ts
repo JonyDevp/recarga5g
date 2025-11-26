01f57b3d725fd246374fd5375ed53febfe7be307
JonyDevp <Jonathansr2029@gmail.com>
2025-11-26 14:28:26 -0600

    refactor(follow-card): remove unused follow card component and template

--- src/app/pages/mas/pages/blog/components/follow-card/follow-card.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-follow-card',
-  imports: [],
-  templateUrl: './follow-card.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class FollowCardComponent { }
