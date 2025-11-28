b7a049e78b2397cb447824ffe2093033fa5b071e
JonyDevp <Jonathansr2029@gmail.com>
2025-11-28 12:06:32 -0600

    refactor(condiciones.component): update meta link

--- src/app/pages/mas/pages/legal/pages/condiciones/condiciones.component.ts
+++ src/app/pages/mas/pages/legal/pages/condiciones/condiciones.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { metaTagModel } from 'src/app/interfaces/meta-tag.model';
-import { MetaTagService } from '@shared/services/meta-tag.service';
+import { SeoService } from '@shared/services/seo.service';
 
 @Component({
   selector: 'app-condiciones',
@@ -11,9 +10,8 @@ import { MetaTagService } from '@shared/services/meta-tag.service';
 })
 export default class CondicionesComponent implements OnInit {
 
-
  private readonly title = inject( Title ); 
- private readonly meta = inject( MetaTagService );
+ private readonly meta = inject( SeoService );
 
   ngOnInit(): void {
     this.title.setTitle('Recarga5g.com: Consulta nuestras condiciones de uso de nuestras plataformas');
@@ -22,7 +20,7 @@ export default class CondicionesComponent implements OnInit {
       description: 'En Recarga5g.com, nos comprometemos a ofrecerte un servicio transparente y seguro. Consulta nuestras condiciones de uso para conocer tus derechos y responsabilidades al utilizar nuestras plataformas.',
       keywords: 'condiciones de uso, términos y condiciones, políticas de uso, Recarga5g.com, servicio seguro, transparencia, derechos del usuario',
       typeContent: 'website',
-      url: 'https://www.recarga5g.com/mas/legal/condiciones',
+      url: '/mas/legal/condiciones',
     
     });
   }
