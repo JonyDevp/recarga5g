9756b77c2f142199a4c4ac356fbacdb387757a93
JonyDevp <Jonathansr2029@gmail.com>
2025-11-28 11:52:48 -0600

    refactor(terminales.component): update meta data for SEO

--- src/app/pages/productos/terminales/terminales.component.ts
+++ src/app/pages/productos/terminales/terminales.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, computed, inject, OnInit } from '@a
 import { Title } from '@angular/platform-browser';
 import { CardProductComponent } from '@feature/components/card-product/card-product.component';
 import { DoubtsComponent } from '@feature/components/doubts/doubts.component';
-import { MetaTagService } from '@shared/services/meta-tag.service';
+import { SeoService } from '@shared/services/seo.service';
 import { ThemesService } from '@shared/services/themes.service';
 
 @Component({
@@ -17,22 +17,22 @@ import { ThemesService } from '@shared/services/themes.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class TerminalesComponent implements OnInit{ 
-  private readonly metaTagService = inject(MetaTagService);
+  private readonly metaTagService = inject(SeoService);
   private readonly title = inject(Title);
 
       theme = inject(ThemesService);
       isDarkTheme = computed(() => this.theme.themeChange());
   
       ngOnInit(): void {
-            this.title.setTitle('Recarga5g.com: Puntos de Venta PV - Compra tu terminal al mejor precio para tu negocio')
+            this.title.setTitle('Recarga5g.com: Compra tu terminal PV al mejor precio para tu negocio')
 
         this.metaTagService.updateMetaTag({
-          title: 'Recarga5g.com: Puntos de Venta PV - Compra tu terminal al mejor precio para tu negocio',
+          title: 'Recarga5g.com: Compra tu terminal PV al mejor precio para tu negocio',
           description:
-            'Adquiere tu terminal para tu negocio en Recarga5G. Cobra recargas, pago de servicios y pines electrónicos y mucho mas. ¡Compra ahora y mejora la experiencia de cobro con tus clientes!',
+            'Adquiere tu terminal para tu negocio sin importar el tipo de giro con Recarga5G. Plataforma para venta de recargas, pago de servicios y pines incluida. ¡Compra ahora y mejora la experiencia de cobro con tus clientes!',
           keywords:
-            'terminales, compra de terminales, ofertas en terminales, Recarga5G terminales, terminales punto de venta, terminales pv, terminales pagaqui, terminales para negocios',
-          url: 'https://www.recarga5g.com/productos/terminales',
+            'terminales, compra de terminales, ofertas en terminales, Recarga5G terminales, terminales punto de venta, terminales pv, terminales pagaqui, terminales para negocios, comprar terminales en línea, terminales económicas, terminales recargas, terminales servicios, terminales pines electrónicos, terminal para negocios, terminales de pago, terminales recarga5g, compra terminales negocios, comprar terminal, comprar pv, adquirir terminales, terminales para comercios, terminales punto de venta recarga5g',
+          url: '/productos/terminales',
           typeContent: 'website'
         });
       }
